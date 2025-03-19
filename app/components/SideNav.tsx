1598040d7de80495500a6bf28ed8de9e2b2cb939
Peterson Perez Ramirez <peterson.perez@ikeasi.com>
2025-03-19 12:53:33 -0400

    Change: tangs A to Link for better perfomance

--- app/components/SideNav.tsx
+++ app/components/SideNav.tsx
@@ -1,26 +1,27 @@
 import { FaFileInvoice, FaHome, FaPowerOff, FaUsers } from "react-icons/fa";
 import { Logo } from "./Logo";
 import NavLinks from "./NavLinks";
+import Link from "next/link";
 
 const SideNav = () => {
     return (
         <div className="flex h-full flex-col px-3 py-4 md:px-2">
-            <a href="#" className="mb-2 text-white flex h-20 items-end justify-start bg-stale-900p-4 md:h-40">
+            <Link href="#" className="mb-2 text-white flex h-20 items-end justify-start bg-slate-900 p-4 md:h-40">
                 <div className="w-32 md:w-40 ">
                     <Logo />
                 </div>
-            </a>
+            </Link>
             <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
                 <NavLinks />
                 <div className="hidden h-auto w-full grow md:block"></div>
-                <a href="/" className="flex h-[48px] grow items-center 
+                <Link href="/" className="flex h-[48px] grow items-center 
                         justify-center gap-2 rounded-md bg-slate-500 p-3 
                         text-lg text-white font-bold hover:bg-slate-400
                         hover:text-white md:flex-none md:justify-start 
                          md:p-2 md:px-3">
                     <FaPowerOff className="w-6" />
                     <p className="hidden md:block">Logout</p>
-                </a>
+                </Link>
             </div>
         </div>
     );
