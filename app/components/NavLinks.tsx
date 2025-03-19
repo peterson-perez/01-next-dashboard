f3f999c16b4838069b68df0a96fd80ff6f27b8d3
Peterson Perez Ramirez <peterson.perez@ikeasi.com>
2025-03-19 12:51:00 -0400

    Add: TwMerge and New urls Links

--- app/components/NavLinks.tsx
+++ app/components/NavLinks.tsx
@@ -1,4 +1,9 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { FaFileInvoice, FaHome, FaUsers } from "react-icons/fa";
+import { twMerge } from 'tailwind-merge'
 
 const links = [
     {
@@ -10,32 +15,32 @@ const links = [
     {
         id: Math.random(),
         name: "Invoices",
-        href: "/invoices",
+        href: "/dashboard/invoices",
         icon: FaFileInvoice
     },
     {
         id: Math.random(),
         name: "Customers",
-        href: "/customers",
+        href: "/dashboard/customers",
         icon: FaUsers
     }
 ];
 
 const NavLinks = () => {
 
+    const pathname = usePathname();
+
     return (
         <>
             {
                 links.map(e => {
                     const LinIcon = e.icon
-                    return <a key={e.id} href={e.href} className="flex h-[48px] grow items-center 
-                                justify-center gap-2 rounded-md bg-slate-500 p-3 
-                                text-lg text-white font-bold hover:bg-slate-400
-                                hover:text-white md:flex-none md:justify-start 
-                                 md:p-2 md:px-3">
+                    return <Link key={e.id} href={e.href} className={twMerge(
+                        "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-slate-700 p-3 text-lg text-white font-bold hover:bg-slate-500 hover:text-white md:flex-none md:justify-start md:p-2 md:px-3",
+                        pathname === e.href && "bg-slate-500")}>
                         <LinIcon className="w-6" />
                         <p className="hidden md:block">{e.name}</p>
-                    </a>
+                    </Link >
                 })
             }
         </>
