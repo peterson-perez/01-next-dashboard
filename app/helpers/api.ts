0352400565b23b4336c2551eb8f7d4f698572fb2
peterson-perez <p.perez2001@hotmail.com>
2025-03-20 10:18:22 -0400

    "Add: fetchRevenuesData"

--- app/helpers/api.ts
+++ app/helpers/api.ts
@@ -1,9 +1,10 @@
-export const fetchCardData = async () => {
+const headers = {
+    "Content-Type": "aplication/json",
+    Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3ZGIwODA4NjFjYzI2MzJhOWQwMWEyYSIsImVtYWlsIjoicGV0ZXJzb25AZ21haWwuY29tIiwibmFtZSI6InBldGVyc29uIiwiaWF0IjoxNzQyNDA3OTcxfQ.2knw9WMl0J0g_ZQQWDTyznl2FBAfgAN5AsaGsweilNw"
+}
 
-    const headers = {
-        "Content-Type": "aplication/json",
-        Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3ZGIwODA4NjFjYzI2MzJhOWQwMWEyYSIsImVtYWlsIjoicGV0ZXJzb25AZ21haWwuY29tIiwibmFtZSI6InBldGVyc29uIiwiaWF0IjoxNzQyNDA3OTcxfQ.2knw9WMl0J0g_ZQQWDTyznl2FBAfgAN5AsaGsweilNw"
-    }
+
+export const fetchCardData = async () => {
 
     try {
 
@@ -33,4 +34,11 @@ export const fetchCardData = async () => {
         console.log("error =>", error);
         throw new Error('Failed to fetch card data.');
     }
+}
+
+export const fetchRevenuesData = async () => {
+    const getRevenues = await fetch(`${process.env.BACKEND_URL}/revenues`, { headers });
+
+    const resultRevenues = await getRevenues.json()
+    return resultRevenues;
 }
