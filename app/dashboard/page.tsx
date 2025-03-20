a2bba0012a417e8164029c14957d0b0a4d8c8bf0
peterson-perez <p.perez2001@hotmail.com>
2025-03-20 09:43:59 -0400

    Add: fetching data and implement the result in the cards

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,19 @@
 import { InfoCard } from "anjrot-components";
 import { bebas } from "../ui/fonts";
+import { fetchCardData } from "../helpers/api";
 
 
-const Dashboard = () => {
+const Dashboard = async () => {
+
+    const { numberOfCustomers, numberOfInvoicesCount, totalPaidInvoices, totalPendingInvoices } = await fetchCardData();
     return (
         <main >
             <h1 className={`${bebas.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-                <InfoCard title="cualquiera" value={34455} type="pending" />
-                <InfoCard title="cualquiera" value={34455} type="pending" />
-                <InfoCard title="cualquiera" value={34455} type="pending" />
-                <InfoCard title="cualquiera" value={34455} type="pending" />
+                <InfoCard title="Collected" value={totalPaidInvoices} type="collected" currency={{ locale: "en-US", currency: "USD" }} />
+                <InfoCard title="Peding" value={totalPendingInvoices} type="pending" currency={{ locale: "en-US", currency: "USD" }} />
+                <InfoCard title="Total Invoices" value={numberOfInvoicesCount} type="invoices" />
+                <InfoCard title="Total Costumers" value={numberOfCustomers} type="customers" />
             </div>
         </main>
     );
---
+++ app/helpers/api.ts
@@ -0,0 +1,36 @@
+export const fetchCardData = async () => {
+
+    const headers = {
+        "Content-Type": "aplication/json",
+        Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3ZGIwODA4NjFjYzI2MzJhOWQwMWEyYSIsImVtYWlsIjoicGV0ZXJzb25AZ21haWwuY29tIiwibmFtZSI6InBldGVyc29uIiwiaWF0IjoxNzQyNDA3OTcxfQ.2knw9WMl0J0g_ZQQWDTyznl2FBAfgAN5AsaGsweilNw"
+    }
+
+    try {
+
+        const [getCostumerCount, getInvoicesCount, getInvoicesStatusCount] = await Promise.all([
+            fetch(`${process.env.BACKEND_URL}/customer/count`, { headers }),
+            fetch(`${process.env.BACKEND_URL}/invoices/count`, { headers }),
+            fetch(`${process.env.BACKEND_URL}/invoices/status-count`, { headers })
+        ])
+
+        const resultCostumerCount = await getCostumerCount.json();
+        const resultInvoicesCount = await getInvoicesCount.json();
+        const resultInvoicesStatusCount = await getInvoicesStatusCount.json();
+
+        const numberOfCustomers = Number(resultCostumerCount ?? "0");
+        const numberOfInvoicesCount = Number(resultInvoicesCount ?? "0");
+        const totalPaidInvoices = resultInvoicesStatusCount.paid ?? "0";
+        const totalPendingInvoices = resultInvoicesStatusCount.pending ?? "0";
+
+        return {
+            numberOfCustomers,
+            numberOfInvoicesCount,
+            totalPaidInvoices,
+            totalPendingInvoices
+        };
+
+    } catch (error) {
+        console.log("error =>", error);
+        throw new Error('Failed to fetch card data.');
+    }
+}
