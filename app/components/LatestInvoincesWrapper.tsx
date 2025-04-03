af6fe2feab806256e96f569aa42ef68dae13b6a0
peterson-perez <p.perez2001@hotmail.com>
2025-04-03 11:06:17 -0400

    add(LatestInvoicesWrapper): add invoices image to the LatestInvoices table

--- app/components/LatestInvoincesWrapper.tsx
+++ app/components/LatestInvoincesWrapper.tsx
@@ -1,11 +1,13 @@
 import { LatestInvoices } from "anjrot-components";
 import { fetchLastestInvoices } from "../helpers/api";
+import Image from "next/image";
 
 const LatestInvoincesWrapper = async () => {
 
     const getLatestInvoinces = await fetchLastestInvoices();
+    console.log(getLatestInvoinces)
      
-    return <LatestInvoices latestInvoices={getLatestInvoinces} className="bg-slate-700" footer={{className: "text-white"}}/>;
+    return <LatestInvoices latestInvoices={getLatestInvoinces} ImgComponent={Image} className="bg-slate-700" footer={{className: "text-white"}}/>;
 };
 
 export default LatestInvoincesWrapper;
