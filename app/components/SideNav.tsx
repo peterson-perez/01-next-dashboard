4b3f65ed74082a5ab941fb1fbdfec9ce6e30d5f2
peterson-perez <p.perez2001@hotmail.com>
2025-04-10 14:39:48 -0400

    add: signout and confing private routes

--- app/components/SideNav.tsx
+++ app/components/SideNav.tsx
@@ -2,6 +2,7 @@ import { FaFileInvoice, FaHome, FaPowerOff, FaUsers } from "react-icons/fa";
 import { Logo } from "./Logo";
 import NavLinks from "./NavLinks";
 import Link from "next/link";
+import { signOut } from "next-auth/react";
 
 const SideNav = () => {
     return (
@@ -14,14 +15,22 @@ const SideNav = () => {
             <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
                 <NavLinks />
                 <div className="hidden h-auto w-full grow md:block"></div>
-                <Link href="/" className="flex h-[48px] grow items-center 
+                <form
+                    action={async () => {
+                        "use server";
+
+                        await signOut({ redirectTo: "/" });
+                    }}
+                >
+                    <button type="submit" className="flex h-[48px] w-full grow items-center 
                         justify-center gap-2 rounded-md bg-slate-500 p-3 
                         text-lg text-white font-bold hover:bg-slate-400
                         hover:text-white md:flex-none md:justify-start 
                          md:p-2 md:px-3">
-                    <FaPowerOff className="w-6" />
-                    <p className="hidden md:block">Logout</p>
-                </Link>
+                        <FaPowerOff className="w-6" />
+                        <p className="hidden md:block">Logout</p>
+                    </button>
+                </form>
             </div>
         </div>
     );
