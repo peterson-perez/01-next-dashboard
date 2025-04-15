bb7d81fdfd74310c8b23678f0b6ebf22048f97b8
peterson-perez <p.perez2001@hotmail.com>
2025-04-15 15:08:10 -0400

    debuggin the pagination

--- app/components/InvoicesWrapper.tsx
+++ app/components/InvoicesWrapper.tsx
@@ -1,5 +1,5 @@
 import { InvoiceTable } from "anjrot-components";
-import { fetchFilteredInvoices } from "../helpers/api";
+import { fetchFilteredInvoices, fetchInvoicesPages } from "../helpers/api";
 import Image from "next/image";
 import { FC } from "react";
 import { fetchDeleteInvoice } from "../helpers/actions";
@@ -9,11 +9,19 @@ interface InvoiceWraperProps {
     page?: number
 }
 
-const InvoicesWrapper: FC<InvoiceWraperProps> = async ({query, page}) => {
+const InvoicesWrapper: FC<InvoiceWraperProps> = async ({ query, page }) => {
+    let currentPage = page
 
-    const getInvoices = await fetchFilteredInvoices(query || "", page);
+    const getInvoicesByName = await fetchFilteredInvoices(query)
 
-    return <InvoiceTable invoices={getInvoices} ImgComponent={Image} className="bg-slate-700" tableHeader={{ className: "text-white" }} deleteAction={fetchDeleteInvoice}/>;
+    if (getInvoicesByName && page && getInvoicesByName.length / 6 < page) {
+        currentPage = 1;
+    }
+
+    const getInvoices = await fetchFilteredInvoices(query || "", currentPage);
+
+
+    return <InvoiceTable invoices={getInvoices} ImgComponent={Image} className="bg-slate-700" tableHeader={{ className: "text-white" }} deleteAction={fetchDeleteInvoice} />;
 };
 
 export default InvoicesWrapper;
